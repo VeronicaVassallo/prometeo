2986e5ed62bf39fb7ccbfa001567986c2874c333
VeronicaVassallo <vvassallo94@gmail.com>
2024-08-27 15:56:14 +0200

    fix error + get name of city from openstreetmap nomination

--- src/components/card/CardComponent.jsx
+++ src/components/card/CardComponent.jsx
@@ -8,7 +8,7 @@ const CardComponent = () => {
 	const [temperature, setTemperature] = useState(0);
 	//user location
 	const [userLocation, setUserLocation] = useState(null); // {latitudine , longitudine}
-	const [city, setCity] = useEffect("");
+	const [city, setCity] = useState("");
 
 	const getUserLocation = () => {
 		if (navigator.geolocation) {
@@ -27,30 +27,60 @@ const CardComponent = () => {
 		}
 	};
 
-	const getCityName = () => {};
+	const getCityName = async () => {
+		if (userLocation) {
+			try {
+				const response = await fetch(
+					`https://nominatim.openstreetmap.org/reverse?format=json&lat=${userLocation.latitude}&lon=${userLocation.longitude}&zoom=10`
+				);
+				const data = await response.json();
+				setCity(
+					data.address.city ||
+						data.address.town ||
+						data.address.village ||
+						"Città non trovata"
+				);
+			} catch (error) {
+				console.log("Error, during fetching the name of city");
+			}
+		} else {
+			console.log("Error, there aren't latitude and longitude");
+		}
+	};
 
 	const getWeatherData = async () => {
 		if (userLocation) {
-			const response = await fetch(
-				`https://api.open-meteo.com/v1/forecast?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&hourly=temperature_2m&temperature_unit=celsius`
-			);
-			const data = await response.json();
-			setTemperature(data);
-			console.log("Data from API", data);
+			try {
+				const response = await fetch(
+					`https://api.open-meteo.com/v1/forecast?latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&hourly=temperature_2m&temperature_unit=celsius`
+				);
+				const data = await response.json();
+				setTemperature(data);
+				console.log("Data from API", data);
+			} catch (error) {
+				console.log("Error fetching weatherData", error);
+			}
 		}
 	};
 
 	useEffect(() => {
 		getUserLocation();
-		getWeatherData();
 	}, []);
+
+	useEffect(() => {
+		if (userLocation) {
+			getCityName();
+			getWeatherData();
+		}
+	}, [userLocation]);
 
 	return (
 		<div>
-			<h3>Roma</h3>
+			<h3>{city}</h3>
 			{temperature ? (
 				<div>
 					<p>Temperatura ora: {temperature.hourly.temperature_2m[14]}°C</p>
+					{/*TO DO: prendere dall'array la  temperatura dell'ora corrente*/}
 				</div>
 			) : (
 				<p>not found</p>
