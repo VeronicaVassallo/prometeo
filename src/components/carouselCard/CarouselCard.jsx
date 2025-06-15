dda8e04778f287a60d5ffd15baa2fd77f20f8bef
Veroniva.Vassallo <vvassallo94@gmail.com>
2025-06-15 23:55:55 +0200

    add swiper for the carousel

--- src/components/carouselCard/CarouselCard.jsx
+++ src/components/carouselCard/CarouselCard.jsx
@@ -4,12 +4,34 @@ import FirstCarouselCard from "../firstCarouselCard/FirstCarouselCard";
 import SecondCarouselCard from "../secondCarouselCard/SecondCarouselCard";
 import ThirdCarouselCard from "../thirdCarouselCard/ThirdCarouselCard";
 
+/*Swiper*/
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
+
+import { FreeMode } from 'swiper/modules';
+
 const CarouselCard = () => {
     return(
         <div className="containerCarousel">
-            <FirstCarouselCard/>
-            <SecondCarouselCard/>
-            <ThirdCarouselCard/>
+            <Swiper
+                spaceBetween={20}
+                slidesPerView={1} // Cambia in base alla larghezza desiderata
+                freeMode={true}
+                grabCursor={true}
+                modules={[FreeMode]}
+            >
+                <SwiperSlide>
+                    <FirstCarouselCard />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <SecondCarouselCard />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <ThirdCarouselCard />
+                </SwiperSlide>
+            </Swiper>
         </div>
     )
 }
